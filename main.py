f545ddd3f2afabcee80c73596a84a6beb3dedf1f
Mark Tabatskyi <vasenka688@gmail.com>
2024-06-14 13:23:27 +0300

    finished eigenvalues and eigenvectors

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-import numpy
+import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.image import imread
 
 
+def find_eigens(matrix):
+    eigenvalues, eigenvectors = np.linalg.eig(matrix)
+    for i in range(len(eigenvalues)):
+        Av = np.dot(matrix, eigenvectors[:, i])
+        lv = eigenvalues[i] * eigenvectors[:, i]
+        if not np.allclose(Av, lv):
+            print(f"A*v != λ*v for eigenvalue λ={eigenvalues[i]} and eigenvector v={eigenvectors[:, i]}")
+    return eigenvalues, eigenvectors
+
+
+print(find_eigens(np.array([[1, 2], [2, 1]])))
